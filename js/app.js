896755a5513d068d4e942aadabb74c65beb60f58
bthipavo <bthipavo@gmail.com>
2017-12-18 20:19:51 -0800

    created search path

--- js/app.js
+++ js/app.js
@@ -1,10 +1,39 @@
-$.ajax({
-    url: 'http://localhost:3000/igdb'
-}).done(function(data) {
-    console.log('data', data);
-    // $('.content').empty();
-    // for (i = 0; i < data.length; i++) {
-    //     $('.content').append('<div>Name: ' + data[i].name + '</div>');
-    // }
-    console.log(data);
-})
+
+var gameData = {};
+var searchOptions = "";
+
+$('#submit').on('click', function() {
+	searchOptions = $('#userInput').val().trim();
+	console.log(searchOptions);
+	$.ajax({
+	    url: 'http://localhost:3000/search',
+	    data: {search: searchOptions},
+	}).done(function(data) {
+	    // console.log('data', data);
+	    var searchResult = $('<div>');
+	    for (var i in data) {
+          			
+                    var searchReturn = $('<p>');
+                    searchReturn.html('Title: ' + data[i].name + '<br/>' +
+                    				  'Rating: ' + data[i].total_rating + '<br/>');
+                    				  // '<img src=' + data.i.images.downsized_still.url);
+                    searchResult.append(searchReturn);
+          	}
+        $('#searchContent').append(searchResult);
+	    console.log(data);
+	});
+});;
+// console.log('http://localhost:3000/search'+searchOptions);
+
+
+
+
+// $.ajax({
+//     url: 'http://localhost:3000/characters'
+// }).done(function(data) {
+//     // console.log('data', data);
+//     console.log("characters");
+//     gameData = data;
+//     console.log(gameData);
+// })
+
--- js/back.js
+++ js/back.js
@@ -3,23 +3,94 @@ const igdb = require('igdb-api-node').default;
 const client = igdb('b0b518b52239e302279cb9e719a371c8');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var querystring = require('querystring');
+var searchText = "";
 
+
+// app.params('getSearchText', function(req, res, next, getSearchText){
+// 	console.log('found search text' + req.query.getSearchText);
+// });
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// console.log("outside ");
+
+
+// app.post('/search?', function (req, res) {
+// 	searchText = req.body.search;
+//     console.log("searching: " + searchText);
+//     res.json({ ok: true });
+
+// });
 // on the request to root (localhost:3000/)
-app.get('/igdb', function (req, res) {
+app.get('/search', function (req, res) {
     // res.send('<b>My</b> first express http server');
     res.setHeader('Access-Control-Allow-Origin', '*');
-
+    console.log(req.query.search);
+    
+    searchText = req.query.search;
+    // searchText = searchText.substring(searchText.indexOf(":")+1);
     client.games({
-        fields: '*', // Return all fields
-        limit: 5, // Limit to 5 results
-        offset: 15 // Index offset for results
+        fields: ['name', 'summary', 'storyline', 'total_rating', 'aggregated_rating', 
+        'first_release_date', 'release_dates.date', 'popularity', 'url', 'category', 'screenshots', 'videos'], // Return all fields
+        limit: 10, // Limit to 5 results
+        scroll: 1, // Index offset for results
+        // search: 'zelda',
+        search: searchText,
+    	order: 'aggregated_rating:desc'
+        
     }).then(response => {
         // response.body contains the parsed JSON response to this query
-        //console.log('response.body', response.body);
+        console.log('response.body', searchText);
         res.send(response.body);
     }).catch(error => {
         throw error;
     });
+});
+
+app.get('/', function (req, res) {
+    // res.send('<b>My</b> first express http server');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    client.games({
+        fields: ['name', 'total_rating', 'aggregated_rating', 'first_release_date', 'release_dates.date'], // Return all fields
+        limit: 10, // Limit to 5 results
+        offset: 15, // Index offset for results
+        // search: 'zelda',
+        filters: { 
+        	'aggregated_rating-gte' : '80',
+        'release_dates.date-gt': '2016-12-31',
+        'release_dates.date-lt': '2018-01-01'
+    	},
+    	order: 'aggregated_rating:desc'
+        
+    }).then(response => {
+        // response.body contains the parsed JSON response to this query
+        // console.log('response.body', response);
+        res.send(response.body);
+    }).catch(error => {
+        throw error;
+    });
+});
+
+app.get('/characters', function (req, res) {
+    // res.send('<b>My</b> first express http server');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    client.characters({
+        fields: '*', // Return all fields
+        limit: 10, // Limit to 5 results
+        offset: 15, // Index offset for results
+        filters: {'gender':'1'}
+        
+    }).then(response => {
+        // response.body contains the parsed JSON response to this query
+        //console.log('response.body', response.body);
+        // res.send(response.body);
+    }).catch(error => {
+        throw error;
+    });
+
 });
 
 // // On localhost:3000/welcome
@@ -34,5 +105,5 @@ app.get('/igdb', function (req, res) {
 
 // start the server in the port 3000 !
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000.');
+    console.log('Example app listening on port 3000.'); 
 });
